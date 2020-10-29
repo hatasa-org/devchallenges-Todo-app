21a1d35eb310f984fd2689798bd3cabc74faf912
Takahiro <t-shinkai00@gamil.com>
2020-10-29 22:43:40 +0900

    complete all tab (referenced from Completed tab by yk-hts)

--- src/pages/All/index.js
+++ src/pages/All/index.js
@@ -1,12 +1,102 @@
-import React from "react";
-import AddTask from "../../components/AddTask";
+import React, { useEffect, useState } from "react";
+import {
+  IonContent,
+  IonPage,
+  IonCheckbox,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonButton,
+} from "@ionic/react";
+import { trashOutline } from "ionicons/icons";
 
 const All = () => {
+  const [tasks, setTasks] = useState([
+    { title: "title1", isCompleted: true },
+    { title: "title2", isCompleted: false },
+    { title: "title3", isCompleted: true },
+    { title: "title4", isCompleted: false },
+    { title: "title5", isCompleted: true },
+    { title: "title6", isCompleted: false },
+  ]);
+  // const [tasks, setTasks] = useState([])
+  // useEffect(()=> {
+  //   if("tasks" in localStorage){
+  //     setTasks(JSON.parse(localStorage.getItem("tasks")))
+  //   }
+  // },[])
+
+  const toggleIsCompleted = (i) => {
+    const tmp = JSON.parse(JSON.stringify(tasks));
+    tmp[i].isCompleted = !tmp[i].isCompleted;
+    setTasks(tmp);
+    // setTasks((prevState) => {
+    //   prevState[i].isCompleted = !prevState[i].isCompleted;
+    //   return [...prevState];
+    // });
+  };
+
+  const deleteTask = (i) => {
+    const tmp = JSON.parse(JSON.stringify(tasks));
+    tmp.splice(i, 1);
+    setTasks(tmp);
+  };
+
+  const deleteAllTask = () => {
+    const tmp = JSON.parse(JSON.stringify(tasks));
+    const afterDelete = tmp.filter((task) => !task.isCompleted);
+    setTasks(afterDelete);
+  };
+
   return (
-    <div>
-      <AddTask />
-      <h1>All</h1>
-    </div>
+    <IonPage>
+      <IonContent>
+        <IonList>
+          {tasks.map(({ title, isCompleted }, i) => {
+            return (
+              <IonItem key={i} lines="full">
+                <IonLabel>{title}</IonLabel>
+                <IonCheckbox
+                  slot="start"
+                  value={title}
+                  checked={isCompleted}
+                  onIonChange={() => {
+                    toggleIsCompleted(i);
+                  }}
+                />
+                <IonButton
+                  fill="clear"
+                  onClick={() => {
+                    deleteTask(i);
+                  }}
+                  color="medium"
+                >
+                  <IonIcon
+                    slot="end"
+                    icon={trashOutline}
+                    style={{ margin: "auto 0" }}
+                  />
+                </IonButton>
+              </IonItem>
+            );
+          })}
+        </IonList>
+        <IonButton
+          type="button"
+          className="ion-float-end"
+          color="danger"
+          onClick={() => deleteAllTask()}
+        >
+          <IonIcon
+            icon={trashOutline}
+            size="small"
+            style={{ marginRight: "8px" }}
+          />
+          delete all
+        </IonButton>
+      </IonContent>
+    </IonPage>
   );
 };
 
